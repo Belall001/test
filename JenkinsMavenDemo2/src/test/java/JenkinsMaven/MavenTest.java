ddbd1247f666dcdacb2f1369e4c22816cd87f43a
Belall <belall.kareemun@rubric.mu>
2017-06-13 13:05:20 +0400

    first time commit all in github

---
+++ JenkinsMavenDemo2/src/test/java/JenkinsMaven/MavenTest.java
@@ -0,0 +1,28 @@
+package JenkinsMaven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class MavenTest {
+	
+	public WebDriver driver;
+	
+	
+	@Test
+	public void TestHello(){
+		
+		System.out.println("Hello world");
+		
+		System.out.println("Hello world2");
+		
+		System.setProperty("geckodriver.exe", "C:/Users/Automation/Desktop/Eclipse workspace/JenkinsDemo/lib/chromedriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("http://www.google.mu");
+		System.out.print(driver.getTitle());
+		driver.close();
+		
+	}
+
+}
